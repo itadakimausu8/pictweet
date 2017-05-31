58500a0dea8242c6e1a037fb6aee5cf8393d887d
DESKTOP-LJICTRB\sumay <sumayasu0301@gmail.com>
2017-05-31 22:34:03 +0900

    Revert "Model and Routing"
    This reverts commit 2a5615733c54b112e0ed4aa61432159de79ef214.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,11 +3,5 @@ class Tweet < ActiveRecord::Base
 #・all,new,save,find,createメソッド(rails)を使用可能にする
 #これらがSQL言語に変換することで代わりにDBを動かしている
   belongs_to :user
-#アソシエーションを定義するために使う。一(Userモデル)対多(Tweetモデル)
-#一対多の関係において自分のモデルが「多」の条件に当てはまるときに使用。
-#記述することでtweets.userの記述を可能にする。
   has_many :comments
-#アソシエーションを定義するために使う。一(Tweetモデル)対多(Commentモデル)
-#一対多の関係において自分のモデルが「一」の条件に当てはまるときに使用。
-#記述することでtweet.commentsの記述を可能にする。
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-# ↑ deviseモジュールの設定を行える。
-# それぞれの詳細はhttp://qiita.com/cigalecigales/items/73d7bd7ec59a001ccd74を参照。
   has_many :tweets
-#アソシエーションを定義するために使う。一(Userモデル)対多(Tweetモデル)
-#一対多の関係において自分のモデルが「一」の条件に当てはまるときに使用。
-#記述することでuser.tweetsの記述を可能にする。
   has_many :comments
-#アソシエーションを定義するために使う。一(Userモデル)対多(Commentモデル)
-#一対多の関係において自分のモデルが「一」の条件に当てはまるときに使用。
-#記述することでuser.commentsの記述を可能にする。
   validates :nickname, presence: true, length: { maximum: 6 }
-#validatesメソッド：モデルクラスによって作られたインスタンスの検証を行うことができる。
-# ↑ の場合nicknameカラムが「存在している」かつ「文字列の長さが６以内に収まる」ときそのインスタンスが存在することを認める。
 end
