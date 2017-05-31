2a5615733c54b112e0ed4aa61432159de79ef214
DESKTOP-LJICTRB\sumay <sumayasu0301@gmail.com>
2017-05-31 22:25:18 +0900

    Model and Routing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :tweet
+  #アソシエーションを定義するために使う。一(Tweetモデル)対多(Commentsモデル)
+  #一対多の関係において自分のモデルが「多」の条件に当てはまるときに使用。
+  #記述することでcomments.tweetの記述を可能にする。
   belongs_to :user
+  #アソシエーションを定義するために使う。一(Userモデル)対多(Commentsモデル)
+  #一対多の関係において自分のモデルが「多」の条件に当てはまるときに使用。
+  #記述することでcomments.userの記述を可能にする。
 end
